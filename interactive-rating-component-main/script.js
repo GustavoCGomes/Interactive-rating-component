6f3cb9656fc3fe0017f36e803a74c390bed1133e
Gustavo Gomes <108428536+GustavoCGomes@users.noreply.github.com>
2022-11-07 23:02:19 -0300

    JavaScript aplicado

---
+++ interactive-rating-component-main/script.js
@@ -0,0 +1,16 @@
+const mainContainer = document.querySelector(".main-container")
+const thanksContainer = document.querySelector(".thanks-you")
+const submitButton = document.getElementById("submit")
+const rating = document.getElementById("rating")
+const rates = document.querySelectorAll(".btn")
+
+submitButton.addEventListener("click", () => {
+    mainContainer.style.display = "none"
+    thanksContainer.classList.remove("hidden")  
+})
+
+rates.forEach((rate) => {
+    rate.addEventListener("click", () => {
+        rating.innerHTML = rate.innerHTML
+    })
+})
